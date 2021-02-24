22137fda030131b9f7e108b9bb77ec235ad06312
MJALBDOUR <mjalbdour@gmail.com>
2021-02-24 21:41:20 +0200

    added Add2 handlerfunc with addition logic

--- src/addition/add2/Add2.go
+++ src/addition/add2/Add2.go
@@ -4,3 +4,15 @@ type TwoNumbers struct {
 	A float64 `json:"a"`
 	B float64 `json:"b"`
 }
+
+func Add2(w http.ResponseWriter, r *http.Request) {
+	var twoNumbers TwoNumbers
+
+	err := json.NewDecoder(r.Body).Decode(&twoNumbers)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", twoNumbers.A + twoNumbers.B)
+}
